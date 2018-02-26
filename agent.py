e8749874b60eea768a2bd355f8085d00d66cf525
Freddie MacGregor <fm8pref9@gmail.com>
2018-02-26 12:06:29 +0000

    running but not working with saving

---
+++ agent.py
@@ -0,0 +1,59 @@
+import tensorflow as tf
+import numpy as np
+
+
+class policy:
+    def __init__(self, H, D, learning_rate):
+
+        # define forward network
+        self.observations = tf.placeholder(tf.float32, [None, D], name="frame_x")
+        self.W1 = tf.get_variable("W1", shape=[D, H], initializer=tf.contrib.layers.xavier_initializer())
+        layer1 = tf.nn.relu(tf.matmul(self.observations, self.W1))
+        self.W2 = tf.get_variable("W2", shape=[H, 1], initializer=tf.contrib.layers.xavier_initializer())
+        logits = tf.matmul(layer1, self.W2)
+        self.probability = tf.nn.sigmoid(logits)
+
+
+
+
+
+        # training stuff
+        self.tvars = tf.trainable_variables()
+        self.input_y = tf.placeholder(tf.float32, [None, 1], name="input_y")
+        self.advantages = tf.placeholder(tf.float32, name="reward_signal")
+        optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+
+        self.W1Grad = tf.placeholder(tf.float32, name="batch_grad1")
+        self.W2Grad = tf.placeholder(tf.float32, name="batch_grad2")
+        batchGrad = [self.W1Grad, self.W2Grad]
+        cross_ent = tf.nn.sigmoid_cross_entropy_with_logits(labels=self.input_y,logits=logits)
+        loss = tf.reduce_mean(tf.multiply(self.advantages,cross_ent))
+        # loglik = tf.log(self.input_y * (self.input_y - self.probability) + (1 - self.input_y) * (self.input_y + self.probability))
+        # loss = -tf.reduce_mean(loglik * self.advantages)
+        self.newGrads = tf.gradients(loss, self.tvars)
+        self.updateGrads = optimizer.apply_gradients(zip(batchGrad, self.tvars))
+
+    def trainPolicyNetwork(self, W1Grad, W2Grad):
+        self.sess.run(self.updateGrads, feed_dict={self.W1Grad: W1Grad, self.W2Grad: W2Grad})
+
+    def calculatePolicyGradients(self, epx, epy, discounted_epr):
+        newGrads= self.sess.run(self.newGrads, feed_dict={self.observations: epx, self.input_y: epy, self.advantages: discounted_epr})
+        return newGrads
+
+    def setSession(self, sess):
+        self.sess = sess
+
+    def getW1(self):
+        return self.sess.run(self.W1)
+
+    def getW2(self):
+        return self.sess.run(self.W2)
+
+    def evaluatePolicy(self, observations):
+        tfprob = self.sess.run(self.probability, feed_dict={self.observations: observations})
+        return tfprob
+
+    def writeWeights(self):
+        weights = self.sess.run(agent.tvars)
+        print(weights)
+        return str(weights)
--- pong_tf.py
+++ pong_tf.py
@@ -1,12 +1,17 @@
 
 import tensorflow as tf
 import numpy as np
+import  agent
 import cloudpickle as pickle
 import gym
+import matplotlib.pyplot as plt
 
-learning_rate = 1e-4
+learning_rate = 1e-3
 gamma = 0.99  # discount factor for reward
 D = 80*80
+resume = True
+
+# np.set_printoptions(threshold='nan')
 
 
 def prepro(I):
@@ -18,53 +23,6 @@ def prepro(I):
     I[I != 0] = 1  # everything else (paddles, ball) just set to 1
     return I.astype(np.float).ravel()
 
-class policy:
-    def __init__(self, H, D):
-
-        # define forward network
-        self.observations = tf.placeholder(tf.float32, [None, D], name="frame_x")
-        self.W1 = tf.get_variable("W1", shape=[D, H], initializer=tf.contrib.layers.xavier_initializer())
-        layer1 = tf.nn.relu(tf.matmul(self.observations, self.W1))
-        self.W2 = tf.get_variable("W2", shape=[H, 1], initializer=tf.contrib.layers.xavier_initializer())
-        score = tf.matmul(layer1, self.W2)
-        self.probability = tf.nn.sigmoid(score)
-
-
-
-
-
-        # training stuff
-        self.tvars = tf.trainable_variables()
-        self.input_y = tf.placeholder(tf.float32, [None, 1], name="input_y")
-        self.advantages = tf.placeholder(tf.float32, name="reward_signal")
-        adam = tf.train.AdamOptimizer(learning_rate=learning_rate)
-
-        self.W1Grad = tf.placeholder(tf.float32, name="batch_grad1")
-        self.W2Grad = tf.placeholder(tf.float32, name="batch_grad2")
-        batchGrad = [self.W1Grad, self.W2Grad]
-        loglik = tf.log(self.input_y * (self.input_y - self.probability) + (1 - self.input_y) * (self.input_y + self.probability))
-        loss = -tf.reduce_mean(loglik * self.advantages)
-        self.newGrads = tf.gradients(loss, self.tvars)
-        self.updateGrads = adam.apply_gradients(zip(batchGrad, self.tvars))
-
-    def trainPolicyNetwork(self, W1Grad, W2Grad):
-        sess.run(self.updateGrads, feed_dict={self.W1Grad: W1Grad, self.W2Grad: W2Grad})
-
-    def calculatePolicyGradients(self, epx, epy, discounted_epr):
-        newGrads= self.sess.run(self.newGrads, feed_dict={self.observations: epx, self.input_y: epy, self.advantages: discounted_epr})
-        return newGrads
-
-    def setSession(self, sess):
-        self.sess = sess
-
-    def evaluatePolicy(self, observations):
-        tfprob = self.sess.run(self.probability, feed_dict={self.observations: observations})
-        return tfprob
-
-    def writeWeights(self):
-        weights = self.sess.run(agent.tvars)
-        print(weights)
-        return str(weights)
 
 def resetGradBuffer(gradBuffer):
     for ix, grad in enumerate(gradBuffer):
@@ -73,6 +31,7 @@ def resetGradBuffer(gradBuffer):
 
 def discount_rewards(r):
     """ take 1D float array of rewards and compute discounted reward """
+
     discounted_r = np.zeros_like(r)
     running_add = 0
     for t in reversed(range(0, r.size)):
@@ -81,58 +40,86 @@ def discount_rewards(r):
         discounted_r[t] = running_add
     return discounted_r
 
-def prepro(I):
+def prepro(I, c):
     """ prepro 210x160x3 uint8 frame into 6400 (80x80) 1D float vector """
+    # if c % 10 == 0:
+    #     plt.imshow(I)
+    #     plt.show()
     I = I[35:195]  # crop
     I = I[::2, ::2, 0]  # downsample by factor of 2
     I[I == 144] = 0  # erase background (background type 1)
     I[I == 109] = 0  # erase background (background type 2)
     I[I != 0] = 1  # everything else (paddles, ball) just set to 1
+    # if c % 10 == 0:
+    #     plt.imshow(I)
+    #     plt.show()
+
     return I.astype(np.float).ravel()
 
+def discount_epr(rs_n):
+    discounted_epr = discount_rewards(rs_n)
+    discounted_epr -= np.mean(discounted_epr)
+    discounted_epr /= np.std(discounted_epr)
+    return discounted_epr
 
 reward_sum = 0
 running_reward = None
 prev_x = None
 env = gym.make('Pong-v0')
 tf.reset_default_graph()
-agent = policy(200, D)
-init = tf.global_variables_initializer()
+agent = agent.policy(200, D, learning_rate)
 episode_number = 0
+reward_sum = 0
+running_total = 0
 xs, ys, dlogps, rs = [], [], [], []
 saver = tf.train.Saver()
+count = 0
+running = True
+
+
 
 with tf.Session() as sess:
-    sess.run(init)
+    if resume:
+        print('resuming')
+        saver.restore(sess, './models/pong_model_3_lr3.ckpt')
+    else:
+        sess.run(tf.global_variables_initializer())
+
     observation = env.reset()
     gradBuffer = sess.run(agent.tvars)
     gradBuffer = resetGradBuffer(gradBuffer)
     agent.setSession(sess)
 
 
-    while True:
+    while running:
         # keep running episodes
-        cur_x = prepro(observation)
+        cur_x = prepro(observation, count)
         x = cur_x - prev_x if prev_x is not None else np.zeros(D)
         prev_x = cur_x
 
+        # if count % 10 == 0:
+        #     print('count is 10')
+        #     reshaped = x.reshape(80,80)
+        #     plt.imshow(reshaped)
+        #     plt.show()
 
         x = np.reshape(x, [1, D])
         xs.append(x)
         probability = agent.evaluatePolicy(x)
+
         action = 2 if np.random.uniform() < probability else 3  # fake label, what does this mean???
         y = 1 if action == 2 else 0
 
         dlogps.append(y - probability)  # this is a regularisation gradient that pushes slighty for the thing that happened to happen if it was likely, and strongly for it to happen again if it was unlikely
         observation, reward, done, info = env.step(action)
+        reward_sum += reward
         rs.append(reward)
         ys.append(y)
 #         calculate next action from observation (get loss at this point?)
 #         step environment
 #         add observations, action, reward to arrays
 
-
-        reward_sum += reward
+        count += 1
         # print('episode %d reward_sum  %f' % (episode_number, reward))
         if done:
             episode_number += 1
@@ -141,27 +128,35 @@ with tf.Session() as sess:
             y_n = np.vstack(ys)
             rs_n = np.vstack(rs)
             dlogps_n = np.vstack(dlogps)
+            print(reward)
 
+            if reward != -1:
+                xpos_n = np.vstack(xs)
+                ypos_n = np.vstack(ys)
+                rposs_n = np.vstack(rs)
+                dlogpposs_n = np.vstack(dlogps)
 
 
             xs, ys, dlogps, rs = [], [], [], []  # reset array memory for next point (in the game of pong)
             # then normalise, so take the mean and divide by standard deviation
             # do dlogps*rs now
-            discounted_epr = discount_rewards(rs_n)
-            discounted_epr -= np.mean(discounted_epr)
-            discounted_epr /= np.std(discounted_epr)
+            # if episode_number % 5 ==0:
+            #     print('rs_n')
+            #     print(rs_n)
+            discounted_epr = discount_epr(rs_n)
+            # if episode_number % 5 == 0:
+            #     print('discounted_epr')
+            #     print(discounted_epr)
+
             grads = discounted_epr * dlogps_n
             # calculate the relevant gradients for the policy network
             tGrad = agent.calculatePolicyGradients(x_n, y_n, grads)
-            print('grad calculated')
 
             if np.sum(tGrad[0] == tGrad[0]) == 0:
                 break
             # aggregate the gradients into the buffer (can just sum them as 2 variables, savesa  dimension)
             for ix, grad in enumerate(tGrad):
                 gradBuffer[ix] += grad
-
-
             # then train the policy network in a batch!
             if episode_number % 20 == 0:
                 agent.trainPolicyNetwork(gradBuffer[0], gradBuffer[1])
@@ -171,8 +166,16 @@ with tf.Session() as sess:
             running_reward = reward_sum if running_reward is None else running_reward * 0.99 + reward_sum * 0.01
             print('resetting env. episode reward total was %f. running mean: %f' % (reward_sum, running_reward))
 
-            if episode_number % 100 == 0: pickle.dump(agent.writeWeights(), open('nn_p_save.p', 'wb'))
-            saver.save(sess, 'models/pong_model_1.ckpt')
+            if reward_sum != -21:
+                point = {'x': x_n, 'y': y_n, 'r': rs_n, 'dlogps': dlogps_n}
+                running = False
+                pickle.dump(point, open('winning_point.p', 'wb'))
+
+            if episode_number % 100 == 0:
+                model = {'W1': agent.getW1(), 'W2': agent.getW2()}
+                pickle.dump(agent.writeWeights(), open('nn_p_save.p', 'wb'))
+                saver.save(sess, 'models/pong_model_3_lr3.ckpt')
+
             reward_sum = 0
             observation = env.reset()  # reset env
             prev_x = None
